0fb39889e28d95b4316e93ef728857653c4d24cd
Faris Chebib <faris@zaneprep.com>
2015-04-15 14:57:05 -0400

    added initial partials

--- static/js/controller.js
+++ static/js/controller.js
@@ -1,3 +1,20 @@
 'use strict';
 
-angular.module('utahPython', ['ui.router']);
+angular.module('utahPython', ['ui.router'])
+  .config(function($stateProvider, $urlRouterProvider){
+    $urlRouterProvider.otherwise("/");
+
+    $stateProvider
+      .state('about', {
+        url: "/about",
+        templateUrl: "about.html"
+      }).
+      .state('meetups' {
+        url: "/meetups",
+        templateUrl: "meetups.html"
+      })
+      .state('presentation', {
+        url: "/presentations",
+        templateUrl: "presentations.html"
+      });
+  });
