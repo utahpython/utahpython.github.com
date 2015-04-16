a6a55ab09d671ed760aad23d5da4aaae86583204
Faris Chebib <faris@zaneprep.com>
2015-04-16 15:01:00 -0400

    updated initial meetup controller

--- static/js/controller.js
+++ static/js/controller.js
@@ -24,4 +24,23 @@ angular.module('utahPython', ['ui.router'])
         url: "/license",
         templateUrl: "license.html"
       });
+  })
+  .controller('MeetupCtrl', function($http) {
+    var meetup = this;
+    meetup.signed_url = "https://api.meetup.com/2/events?offset=0&format=json&limited_events=False&group_urlname=UtahPython&page=200&fields=&order=time&desc=false&status=upcoming&sig_id=10704711&sig=7eb0b440ac115c04d9133423ec0eb94c8d6ded89"
+
+    meetup.events = [];
+
+    meetup.loadGames = function loadGames () {
+      $http.get(meetup.signed_url)
+        .then(function success(result) {
+          meetup.events = result.data;
+          console.log(meetup.events);
+        }, function error(error) {
+          console.log(error);
+        });  // .then promises to get games
+    };
+
+    meetup.loadGames();
+
   });
--- upcoming.md
+++ upcoming.md
@@ -4,6 +4,19 @@ title: "Meetups"
 upcoming-nav: True
 ---
 
-### Meetups
+## Meetups
 
-Hello
+Utah Python currently hosts 3 different meetups around Utah.
+
+### Salt Lake City &amp; Draper
+
+<a class="btn btn-success" href="http://www.meetup.com/UtahPython/"
+	target="_blank" 
+	role="button">
+	Join SLC’s Meetup Group
+  <span class="glyphicon glyphicon-new-window"></span>
+</a>
+
+{% include meetuptmpl.html %}
+
+### Logan
