9e2b00288c2ff21fb71623f28ba9a84b0360a777
Faris Chebib <faris@zaneprep.com>
2015-04-20 17:21:56 -0400

    sketching out controller

--- static/js/controller.js
+++ static/js/controller.js
@@ -27,20 +27,22 @@ angular.module('utahPython', ['ui.router'])
   })
   .controller('MeetupCtrl', function($http) {
     var meetup = this;
+    // The signed url shouldn't contains any provate key information
+    // It's created by using the meetup js api features
     meetup.signed_url = "https://api.meetup.com/2/events?offset=0&format=json&limited_events=False&group_urlname=UtahPython&page=200&fields=&order=time&desc=false&status=upcoming&sig_id=10704711&sig=7eb0b440ac115c04d9133423ec0eb94c8d6ded89"
 
     meetup.events = [];
 
-    meetup.loadGames = function loadGames () {
+    meetup.loadMeetups = function loadGames () {
       $http.get(meetup.signed_url)
         .then(function success(result) {
           meetup.events = result.data;
           console.log(meetup.events);
         }, function error(error) {
           console.log(error);
-        });  // .then promises to get games
+        });
     };
 
-    meetup.loadGames();
+    meetup.loadMeetups();
 
   });
