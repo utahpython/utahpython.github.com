baa802e987ef0ffeb31a6ecfe8c0ca31bfeb642b
Faris Chebib <faris@zaneprep.com>
2015-04-20 18:26:18 -0400

    added robots.txt and debug page

--- static/js/controller.js
+++ static/js/controller.js
@@ -33,6 +33,7 @@ angular.module('utahPython', ['ui.router'])
     meetup.signed_url = "https://api.meetup.com/2/events?offset=0&format=json&limited_events=False&group_urlname=UtahPython&page=200&fields=&order=time&desc=false&status=upcoming&sig_id=10704711&sig=7eb0b440ac115c04d9133423ec0eb94c8d6ded89"
 
     meetup.events = [];
+    meetup.hello = "Hello world";
 
     meetup.loadMeetups = function loadGames () {
       $http.get(meetup.signed_url)
